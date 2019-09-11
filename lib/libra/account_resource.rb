0dc66a3fa7bc0e176e5b2c6dbaebe196115c4c83
yuan <yuanxinyu.hangzhou@gmail.com>
2019-09-11 08:16:08 +0800

    add EventHandle support

--- lib/libra/account_resource.rb
+++ lib/libra/account_resource.rb
@@ -4,6 +4,13 @@ module Libra
   	define_field :blob, {} 
   end
 
+  EVENT_KEY_LENGTH = 32
+
+  class EventHandle < Canoser::Struct
+    define_field :count, Canoser::Uint64
+    define_field :key, [Canoser::Uint8] #EVENT_KEY_LENGTH
+  end
+
 # pub struct AccountResource {
 #     balance: u64,
 #     sequence_number: u64,
@@ -31,8 +38,8 @@ module Libra
   	define_field :authentication_key, [Canoser::Uint8]
   	define_field :balance, Canoser::Uint64
   	define_field :delegated_withdrawal_capability, Canoser::Bool
-  	define_field :received_events_count, Canoser::Uint64
-  	define_field :sent_events_count, Canoser::Uint64
+  	define_field :received_events, EventHandle
+  	define_field :sent_events, EventHandle
   	define_field :sequence_number, Canoser::Uint64
   end  
 end
